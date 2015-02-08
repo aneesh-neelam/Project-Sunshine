9e86bf54e2fe0fe920c66f2142b9131cbef3e281
aneesh-neelam <neelam.aneesh@gmail.com>
2015-02-08 21:01:34 +0530

    Created Adapter for ListView

--- app/src/main/java/me/aneeshneelam/sunshine/fragment/PlaceholderFragment.java
+++ app/src/main/java/me/aneeshneelam/sunshine/fragment/PlaceholderFragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import me.aneeshneelam.sunshine.R;
 
@@ -20,6 +25,19 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] dummyForecast = {
+                "Monday - Dummy Weather",
+                "Tuesday - Dummy Weather",
+                "Wednesday - Dummy Weather",
+                "Thursday - Dummy Weather",
+                "Friday - Dummy Weather",
+                "Saturday - Dummy Weather",
+                "Sunday - Dummy Weather",
+        };
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(dummyForecast));
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
         return rootView;
     }
 }
