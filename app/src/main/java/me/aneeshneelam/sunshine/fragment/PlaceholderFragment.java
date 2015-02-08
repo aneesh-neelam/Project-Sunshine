7592391d61ad9be6d99e7902d9dcccbeac6ba25f
aneesh-neelam <neelam.aneesh@gmail.com>
2015-02-08 21:01:53 +0530

    Attached Adapter to ListView

--- app/src/main/java/me/aneeshneelam/sunshine/fragment/PlaceholderFragment.java
+++ app/src/main/java/me/aneeshneelam/sunshine/fragment/PlaceholderFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +38,8 @@ public class PlaceholderFragment extends Fragment {
         };
         List<String> weekForecast = new ArrayList<>(Arrays.asList(dummyForecast));
         ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
 
         return rootView;
     }
