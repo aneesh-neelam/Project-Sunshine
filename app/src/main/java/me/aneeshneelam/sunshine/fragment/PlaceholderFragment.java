1aaa880e2b04ef56838d79d475eb502e36498bb7
aneesh-neelam <neelam.aneesh@gmail.com>
2015-02-08 18:09:32 +0530

    Initial App`

---
+++ app/src/main/java/me/aneeshneelam/sunshine/fragment/PlaceholderFragment.java
@@ -0,0 +1,25 @@
+package me.aneeshneelam.sunshine.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.aneeshneelam.sunshine.R;
+
+/**
+ * Created by Aneesh Neelam <neelam.aneesh@gmail.com>
+ */
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
