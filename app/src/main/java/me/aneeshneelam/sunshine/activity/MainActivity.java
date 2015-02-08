10ea3439808c7d5c97767fe0369e07fe70f296a3
aneesh-neelam <neelam.aneesh@gmail.com>
2015-02-08 18:10:07 +0530

    Initial App

--- app/src/main/java/me/aneeshneelam/sunshine/activity/MainActivity.java
+++ app/src/main/java/me/aneeshneelam/sunshine/activity/MainActivity.java
@@ -1,8 +1,7 @@
 package me.aneeshneelam.sunshine.activity;
 
-import android.support.v7.app.ActionBarActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
