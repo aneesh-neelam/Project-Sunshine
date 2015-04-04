b42284e077345979905dbd532a48702317dd282e
aneesh-neelam <neelam.aneesh@gmail.com>
2015-04-04 20:17:44 +0530

    Fixed DetailFragment

--- app/src/main/java/me/aneeshneelam/sunshine/fragment/DetailsFragment.java
+++ app/src/main/java/me/aneeshneelam/sunshine/fragment/DetailsFragment.java
@@ -12,25 +12,22 @@ import me.aneeshneelam.sunshine.R;
 
 public class DetailsFragment extends Fragment {
 
-    public static class PlaceholderFragment extends Fragment {
+    Intent intent;
+    private String forecastStr;
 
-        Intent intent;
-        private String forecastStr;
-
-        public PlaceholderFragment() {
-            intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-            }
+    public DetailsFragment() {
+        intent = getActivity().getIntent();
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
         }
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            TextView detailTextView = (TextView) rootView.findViewById(R.id.detail_text);
-            detailTextView.setText(forecastStr);
-            return rootView;
-        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        TextView detailTextView = (TextView) rootView.findViewById(R.id.detail_text);
+        detailTextView.setText(forecastStr);
+        return rootView;
     }
 }
